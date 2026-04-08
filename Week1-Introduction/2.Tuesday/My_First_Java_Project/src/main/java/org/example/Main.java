f83539a580c8eb0b1a23bc1a86c780d9b07d587e
Jonathan Hop <yithril@gmail.com>
2026-04-08 08:00:52 -0400

    wednesday updates

---
+++ Week1-Introduction/2.Tuesday/My_First_Java_Project/src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Float, strings, booleans, int, long, double, char
+        /*
+        Now I can write a really big comment
+        on more than one line and its cool
+        */
+        //Declaring a variable
+        //1. What kind of data is it?
+        //2. Name the variable what you want
+        //3. = and then give it a starting value.
+        String productName = "Kleenex";
+
+        //True or false, up or down, is or isn't
+        boolean isPremiumMember = true;
+
+        //numbers are more interesting
+        //whole numbers, numbers with no decimals int/long
+        int age = 42;
+        long starsInGalaxy = 10_000_000;
+
+        //numbers with fractions
+        float baggageWeight = 75.2f;
+        double temperature = 98.6;
+
+        //single letters
+        char letterGrade = 'A';
+
+        //rules of variables and declaring them
+        //Here we don't declare the variable, we change it
+        letterGrade = 'B';
+
+        //No mix and match!
+        //If its a string, its a string for life
+    }
+}
