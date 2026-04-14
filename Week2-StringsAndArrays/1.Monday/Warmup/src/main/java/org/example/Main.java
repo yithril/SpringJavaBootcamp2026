c394f1b6897c88e70bb45ef7f713929a03101c46
Jonathan Hop <yithril@gmail.com>
2026-04-14 00:26:55 -0400

    tuesday activities

--- Week2-StringsAndArrays/1.Monday/Warmup/src/main/java/org/example/Main.java
+++ Week2-StringsAndArrays/1.Monday/Warmup/src/main/java/org/example/Main.java
@@ -14,16 +14,16 @@ public class Main {
 
         // Direct comparison using ==
         //System.out.println("Comparing s1 and s2 with == : " + (s1 == s2));
-        System.out.println("Comparing s1 and s3 with == : " + (s1 == s3));
+        //System.out.println("Comparing s1 and s3 with == : " + (s1 == s3));
 
         // Using .equals() and .equalsIgnoreCase()
         //System.out.println("Comparing s1 and s3 with .equals() : " + s1.equals(s3));
-        System.out.println("Comparing s1 and s3 with .equalsIgnoreCase() : " + s1.equalsIgnoreCase(s3));
+        //System.out.println("Comparing s1 and s3 with .equalsIgnoreCase() : " + s1.equalsIgnoreCase(s3));
 
         //Look up what System.identityHashCode() does.
         //Using that information, hypothesize WHY == doesn't work but .equals does.
-        System.out.println("HashCode of s1: " + System.identityHashCode(s1));
-        System.out.println("HashCode of s2: " + System.identityHashCode(s2));
-        System.out.println("HashCode of s3: " + System.identityHashCode(s3));
+        //System.out.println("HashCode of s1: " + System.identityHashCode(s1));
+        //System.out.println("HashCode of s2: " + System.identityHashCode(s2));
+        //System.out.println("HashCode of s3: " + System.identityHashCode(s3));
     }
 }
